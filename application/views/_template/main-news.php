80b1cbd441ab102f5345b34435612650f49625ee
artem zalescky <artemzalescky@gmail.com>
2016-12-03 13:41:28 +0300

    add new news

--- application/views/_template/main-news.php
+++ application/views/_template/main-news.php
@@ -4,6 +4,26 @@
 <div class="blog-section-grids">
 
     <div class="col-md-4 blog-section-grid">
+        <?php $ph->image("news/main_news/marketing.jpg" , ['class' => 'product-image']) ?>
+        <div class="blog-title">
+            <h4 class="text-center">На здании Физико-технического института открыта мемориальная доска</h4>
+        </div>
+        <p>
+            <?php
+            $ph->cut_text('1 декабря 2016 года на здании Физико-технического института открыта мемориальная доска памяти академика НАН Беларуси Станислава Александровича АСТАПЧИКА − известного ученого-материаловеда, заслуженного деятеля науки
+            Республики Беларусь, лауреата Государственной премии СССР в области науки и техники. ', 150)
+                ->text('...');
+            ?>
+        </p>
+        <?php
+        $ph->link_open('news#LG', ['class' => 'btn btn-default'])
+            ->tag('span', null, ['class' => 'glyphicon glyphicon-list'])
+            ->text(' Подробнее')
+            ->link_close('a');
+        ?>
+    </div>
+
+    <div class="col-md-4 blog-section-grid">
         <?php $ph->image("news/main_news/4.jpg" , ['class' => 'product-image']) ?>
         <div class="blog-title">
             <h4 class="text-center">ФТИ НАН Беларуси посетила делегация из компании LG Electronics</h4>
@@ -25,20 +45,20 @@
     </div>
 
     <div class="col-md-4 blog-section-grid">
-        <?php $ph->image("news/main_news/konference.jpg" , ['class' => 'product-image']) ?>
+        <?php $ph->image("news/main_news/5.jpg" , ['class' => 'product-image']) ?>
         <div class="blog-title">
-            <h4 class="text-center">22-25 ноября 2016 года Международная научная конференция</h4>
+            <h4 class="text-center">ФТИ НАН Беларуси посетили представители предприятий г. Калуга</h4>
         </div>
 
         <p>
             <?php
-            $ph->cut_text('22-25 ноября 2016 года в ГО "НПЦ НАН Беларуси по материаловедению" по адресу г. Минск,
-				ул. П. Бровки, 19 состоятся  2 конференции - VII Международная научная конференция', 149)
+            $ph->cut_text('21.11.2016 года Физико-технический институт НАН Беларуси посетили представители предприятий г. Калуга (ООО «ЭРГА», ЗАО «ЭКОН», ООО «Энергомаш-Калуга», ООО «НПО «Геоэнергетика» и др. ) и Калужской торгово-промышленной палаты (Россия).
+                В рамках научных докладов, переговоров и презентаций со стороны российских партнеров был проявлен интерес к следующим разработкам:', 149)
                 ->text('...');
             ?>
         </p>
         <?php
-        $ph->link_open('delegacija', ['class' => 'btn btn-default','style' => 'target="_blank'])
+        $ph->link_open('news#kaluga', ['class' => 'btn btn-default','style' => 'target="_blank'])
             ->tag('span', null, ['class' => 'glyphicon glyphicon-list'])
             ->text(' Подробнее')
             ->link_close('a');
@@ -46,33 +66,33 @@
     </div>
 
 
-
+<!--
     <div class="col-md-4 blog-section-grid">
-        <?php $ph->image("news/main_news/1.jpg" , ['class' => 'product-image']) ?>
+        <?php //$ph->image("news/main_news/1.jpg" , ['class' => 'product-image']) ?>
         <div class="blog-title">
             <h4 class="text-center">ФТИ посетила делагация</h4>
         </div>
         <p>
             <?php
-            $ph->cut_text('С 8 по 12 ноября 2016г Физико-технический институт посетила делегация Института
-                 исследования металлов Китайской Академии наук (Institute of Metal Research, Chinese Academy of
-                 Sciences (IMR CAS)) из г.Шеньян (Shenyang).
-                Тематика нашего сотрудничества относится к области пластической деформации металлов и посвящена
-                экспериментам и моделированию процессов пластической деформации и структурообразования при гидроударной
-                штамповке (hydroforming) различных деталей из листовых и трубчатых заготовок, а также научным и
-                 практическим аспектам поперечно-клиновой прокатки.', 165)
-                ->text('...');
+            //$ph->cut_text('С 8 по 12 ноября 2016г Физико-технический институт посетила делегация Института
+            //    исследования металлов Китайской Академии наук (Institute of Metal Research, Chinese Academy of
+            //     Sciences (IMR CAS)) из г.Шеньян (Shenyang).
+            //    Тематика нашего сотрудничества относится к области пластической деформации металлов и посвящена
+            //    экспериментам и моделированию процессов пластической деформации и структурообразования при гидроударной
+            //    штамповке (hydroforming) различных деталей из листовых и трубчатых заготовок, а также научным и
+            //     практическим аспектам поперечно-клиновой прокатки.', 165)
+            //    ->text('...');
             ?>
         </p>
         <?php
-        $ph->link_open('news#Delegation-From-China', ['class' => 'btn btn-default'])
-            ->tag('span', null, ['class' => 'glyphicon glyphicon-list'])
-            ->text(' Подробнее')
-            ->link_close('a');
+        //$ph->link_open('news#Delegation-From-China', ['class' => 'btn btn-default'])
+        //    ->tag('span', null, ['class' => 'glyphicon glyphicon-list'])
+        //    ->text(' Подробнее')
+        //    ->link_close('a');
         ?>
     </div>
 
-
+-->
 
 
 </div>
